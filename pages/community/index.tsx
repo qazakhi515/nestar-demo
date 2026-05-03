785071b0a7bc1613e2bb24142b23a9a6d810a7e7
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-03 12:55:26 +0500

    feat: organize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+const CommunityList = () => {
+  // const [title, setTitle] = useState<string>("hello");
+  return (
+    <div>
+      {" "}
+      Community List{""}
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        {" "}
+        Press{" "}
+      </button>
+    </div>
+  );
+};
+export default CommunityList;
---
+++ pages/property/index.tsx
@@ -0,0 +1,5 @@
+const PropertyList = () => {
+  console.log("PROPERTY component - APP router");
+  return <div> Property List </div>;
+};
+export default PropertyList;
