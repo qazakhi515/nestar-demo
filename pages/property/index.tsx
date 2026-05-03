c1ce89bc52a4321f029d34470164eff3c9841eee
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-03 14:47:32 +0500

    fix: fixed

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -3,7 +3,6 @@ import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const PropertyList: NextPage = () => {
-  console.log("PROPERT LIST COMPONENT _ PAGES ROUTER");
   return <Container>PROPERTY LIST</Container>;
 };
 
