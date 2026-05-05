01b71f69478255f1a07a34c0de995d8e2a4f06d8
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-06 00:30:29 +0500

    feat: modify home page components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,25 @@
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import Advertisement from "../libs/components/homepage/Advertisement";
+import PopularProperties from "../libs/components/homepage/PopularProperties";
+import TopAgents from "../libs/components/homepage/TopAgents";
+import TopProperties from "../libs/components/homepage/TopProperties";
+import TrendProperties from "../libs/components/homepage/TrendPropeties";
+import withLayoutMain from "../libs/components/layout/LayoutHome";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Trend Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Advertisment</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-      </Stack>
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
+
 export default withLayoutMain(Home);
