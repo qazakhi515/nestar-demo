2077bbb612a14c00863dc81d4ed9aa6fcbbcadb4
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-08 15:56:04 +0500

    feat:propert list publishing

---
+++ libs/config.ts
@@ -0,0 +1,6 @@
+export const propertySquare = [
+  50, 70, 90, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300,
+];
+
+export const propertyTypes = ["APARTMENT", "VILLA", "STUDIO", "HOUSE"];
+export const cities = ["SEOUL", "BUSAN", "DAEGU", "INCHEON"];
