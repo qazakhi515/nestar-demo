438130f480227c708279aa2e3d52fd43f3d07b6c
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-10 10:56:06 +0500

    feat: develop apollo client connection

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,14 +1,14 @@
-// import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
-// // No Subscription required for develop process
+// No Subscription required for develop process
 
-// const httpLink = createHttpLink({
-//   uri: "http://localhost:3007/graphql",
-// });
+const httpLink = createHttpLink({
+  uri: "http://localhost:3007/graphql",
+});
 
-// const client = new ApolloClient({
-//   link: httpLink,
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
-// export default client;
+export default client;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,31 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesfetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 2,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData=>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE </Stack>;
