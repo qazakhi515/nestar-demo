566c84f0ccee3efe402d0e5cba0cbe7d13a3d2ce
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-05-10 10:01:16 +0500

    feat: develop mobile version

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,13 +1,19 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 const Community: NextPage = () => {
   const [title, setTitle] = useState<string>("hello");
-  return (
-    <div style={{ margin: "20 px 0" }}>
-      <Stack className="container">COMMUNITY LIST</Stack>
-    </div>
-  );
+  const device = useDeviceDetect();
+  if (device === "mobile") {
+    return <Stack>COMMUNITY DETAIL MOBILE </Stack>;
+  } else {
+    return (
+      <div style={{ margin: "20 px 0" }}>
+        <Stack className="container">COMMUNITY LIST</Stack>
+      </div>
+    );
+  }
 };
 export default withLayoutBasic(Community);
